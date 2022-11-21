c0b18002d462bc27492efdf229a06bfbc9941eb4
Ilija Mihajlovic <iilija879a@gmail.com>
2022-11-21 11:29:30 +0100

    test

--- main.go
+++ main.go
@@ -39,34 +39,30 @@ func main(){
 	ansC := make(chan string)
 
 
-	problemLoop:
+problemLoop:
+	for i, p := range problems {
+		var answer string
+		fmt.Printf("Problem %d: %s=", i+1, p.q)
 
-		for i, p := range problems {
-			fmt.Println(fmt.Sprintf("%v", p))
-			var answer string
-			fmt.Printf("Problem %d: %s=", i+1, p.q)
-
-			go func() {
-				fmt.Scanf("%s", &answer)
-				ansC <- answer
-			}()
-			select{
-			case <-tObj.C:
-				fmt.Println()
-				break problemLoop
-			case iAns := <-ansC:
-				if iAns == p.a {
-					correctAns++
-				}
-				if i == len(problems) - 1 {
-					close(ansC)
-				}
+		go func() {
+			fmt.Scanf("%s", &answer)
+			ansC <-answer
+		}()
+		select{
+		case <-tObj.C:
+			fmt.Println("\nYou ran out of time")
+			break problemLoop
+		case iAns := <-ansC:
+			if iAns == p.a {
+				correctAns++
+			}
+			if i == len(problems) - 1 {
+				close(ansC)
 			}
 		}
+	}
 
-		fmt.Printf("Your result is %d out of %d\n", correctAns, len(problems))
-		fmt.Printf("Press enter to exit")
-		<-ansC
+	fmt.Printf("Your result is %d out of %d\n", correctAns, len(problems))
 }
 
 func parseProblem(lines [][]string) []problem {
